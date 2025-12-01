07e70ec78935cfd37f8d2009ea273b4411a65e39
Ting-Hsuan Chen <Ting-Hsuan.Chen@plantandfood.co.nz>
2025-12-01 16:32:03 +1300

    move the pub folder to a separate repo

--- pub/script/cal_gc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Navigate to the cloned repository of TEgenomeSimulator
-WRK=$(pwd)
-ml conda
-conda activate hraaxt_emboss
-cd $OUT/TEgenomeSimulator_tair10_ch1_m2_result 
-gc=$(cat GCF_000001735.4_TAIR10.1_genomic_chr1.fna.masked.reformatted.nonTE.emptfixed | infoseq -auto -only -name -length -pgc stdin | awk 'NR>1{print $3}')
